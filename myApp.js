a09913b9bc950f89eede8e4e6a12b13657ab27ae
kroy963 <kroy963@gmail.com>
2021-06-20 23:41:43 +0530

    first commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 
+// app.use(express.static('views'))
+app.use(express.static('/public'))
+app.use('/public',express.static(__dirname+'/public'))
 
-
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/views/index.html')
+})
 
 
 
